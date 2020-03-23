1a79187a97b26da2b40c8e5fb11fab4fcb54be64
colinSeven8 <ccnorse80@gmail.com>
2020-03-22 21:33:08 -0600

    Connected endpoints

--- mern-app/backend/server.js
+++ mern-app/backend/server.js
@@ -1,24 +1,30 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); //Helps us connect to Mongo
 
 require("dotenv").config();
 
 // Server
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors());
 app.use(express.json()); // Allow to parse json
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB batacase connection established successfully!");
+    console.log("MongoDB database connection established successfully!");
 })
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
